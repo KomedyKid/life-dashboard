c5cae0b5c04b60c838d0e78586afb13e7bfdfa13
Salim Al-Yahyaei <salim.yahyaei@gmail.com>
2026-08-06 13:03:04 +0300

    asset class

---
+++ asset.cpp
@@ -0,0 +1,18 @@
+class Asset {
+
+  private: 
+    double value;
+
+  public:
+
+    double getValue() const{
+      return value;
+    }
+
+    void setValue(double setValue) {
+      value = setValue;
+    } 
+
+
+
+  };
