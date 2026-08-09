3dea93d05edf7ac00eadbbb450162bf3a034ac7a
Salim Al-Yahyaei <salim.yahyaei@gmail.com>
2026-08-09 13:16:49 +0300

    price history header

--- include/priceHistory.hpp
+++ include/priceHistory.hpp
@@ -1,1 +1,26 @@
-struct priceHistory {};
+#include <ctime>
+#include <vector>
+using namespace std;
+
+class priceHistory {
+
+private:
+  struct pricePoint {
+    time_t date;
+    double price;
+  };
+
+  vector<pricePoint> dates;
+
+  u_int size;
+
+  void sort();
+
+public:
+  void insertPoint(pricePoint p);
+
+  void deletePoint(time_t t);
+
+  vector<pricePoint> returnPoints();
+  vector<pricePoint> returnPoints(u_int x);
+};
